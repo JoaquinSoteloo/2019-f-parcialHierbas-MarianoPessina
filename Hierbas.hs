4a27b6126f30785736dc342ce67fed7375660bc9
Mariano <mapessina@gmail.com>
2019-06-06 12:01:37 -0300

    corrigiendo experimentos

--- Hierbas.hs
+++ Hierbas.hs
@@ -1,3 +1,4 @@
+-- Mariano Pessina
 module Hierbas where
 
 import Text.Show.Functions
@@ -22,7 +23,7 @@ cerebro = UnRaton {
     nombre = "cerebro",
     edad = 9,
     peso = 0.2,
-    enfermedades = ["brucelosis","sarampión","tuberculosis"]
+    enfermedades = ["brucelosis","sarampion","tuberculosis"]
 }
 
 bicenterrata  :: Raton
@@ -55,7 +56,7 @@ hierbaVerde :: String -> Hierba
 hierbaVerde terminacion raton = raton {enfermedades = filter (terminaCon terminacion) (enfermedades raton)}
 
 terminaCon :: String -> String -> Bool
-terminaCon terminacion enfermedad =  
+terminaCon terminacion enfermedad = not((reverse terminacion) == take (length terminacion) (reverse enfermedad)) 
 
 alcachofa :: Hierba
 alcachofa raton = perderPeso ((peso raton) * (coeficiente raton)) raton
@@ -87,21 +88,27 @@ eliminarEnfermedades cantidadDeLetrasMax raton = raton {enfermedades = (filter (
 type Medicamento = Raton -> Raton
 
 pondsAntiAge :: Medicamento
-pondsAntiAge = hierbaBuena.hierbaBuena.hierbaBuena.alcachofa
+pondsAntiAge = composicionDeLista [hierbaBuena,hierbaBuena,hierbaBuena,alcachofa]
 
---reduceFatFast :: Int -> Hierba -> Medicamento
---reduceFatFast 0 hierbaVerde = hierbaVerde
---reduceFatFast potencia hierbaVerde = reduceFatFast (potencia-1) ((.)alcachofa)
+reduceFatFast :: Int -> Medicamento
+reduceFatFast potencia = composicionDeLista (listaDeHierbas potencia)
 
---pdepCilina :: [String] -> 
---pdepCilina sufijosInfecciosas =
---sufijosInfecciosas = [ "sis", "itis", "emia", "cocos"]
+listaDeHierbas :: Int -> [Hierba]
+listaDeHierbas potencia = (replicate potencia alcachofa) ++ [(hierbaVerde "obesidad")]
 
+pdepCilina :: [String] -> Medicamento
+pdepCilina sufijosInfecciosas = composicionDeLista (map (hierbaVerde) sufijosInfecciosas)
+sufijosInfecciosas = ["sis", "itis", "emia", "cocos"]
+
+composicionDeLista :: [Hierba] -> (Raton -> Raton)
+composicionDeLista [] = id
+composicionDeLista (x:xs) = x.(composicionDeLista xs)
 -- 4 experimentos 
 --a
 cantidadIdeal :: (Num a, Enum a) => (a -> Bool) -> a
 cantidadIdeal condicion = head (filter condicion [1..])
 --b
+
 lograEstabilizar :: Medicamento -> [Raton] -> Bool
 lograEstabilizar medicamento ratones = ningunoConSobrepeso(medicarRatones) && menosDe3Enfermedades(medicarRatones)
 
@@ -114,7 +121,7 @@ ningunoConSobrepeso ratones = all ((<1).peso) ratones
 menosDe3Enfermedades :: [Raton] -> Bool
 menosDe3Enfermedades ratones = all ((<3).(length enfermedades))
 --c
-cantidadIdeal (lograEstabilizar (reduceFatFast potencia hierbaVerde) comunidad) 
+--cantidadIdeal (lograEstabilizar (reduceFatFast potencia hierbaVerde) comunidad) 
 
 --5
 {-a
