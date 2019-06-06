f465e2e6837bdbb6073fe2370286cedfce2c7206
Mariano <mapessina@gmail.com>
2019-06-06 10:10:50 -0300

    hierbas definidas

--- Hierbas.hs
+++ Hierbas.hs
@@ -6,7 +6,7 @@ import Data.List
 type Nombre = String
 type Edad = Float
 type Peso = Float
-type Enfermedades = []
+type Enfermedades = [String]
 
 data Raton = UnRaton {
     nombre :: Nombre,
@@ -22,7 +22,7 @@ cerebro = UnRaton {
     nombre = "cerebro",
     edad = 9,
     peso = 0.2,
-    enfermedades = [brucelosis,sarampión,tuberculosis]
+    enfermedades = ["brucelosis","sarampión","tuberculosis"]
 }
 
 bicenterrata  :: Raton
@@ -38,6 +38,48 @@ huesudo  = UnRaton {
     nombre = "huesudo ",
     edad = 4,
     peso = 10,
-    enfermedades = [altaObesidad,sinusitis]
+    enfermedades = ["altaObesidad","sinusitis"]
 }
+
+--modelado de hierbas
+
+type Hierba = (Raton -> Raton)
+
+hierbaBuena :: Hierba
+hierbaBuena raton = rejuvenecer (sqrt (edad raton)) raton
+
+rejuvenecer :: Edad -> Raton -> Raton
+rejuvenecer anios raton = raton {edad = anios}
+
+--hierbaVerde :: String -> Hierba
+--hierbaVerde terminacion raton = (terminaCon terminacion raton)
+
+
+
+alcachofa :: Hierba
+alcachofa raton = perderPeso ((peso raton) * (coeficiente raton)) raton
+
+coeficiente :: Raton -> Float
+coeficiente raton | peso raton > 2 = 0.2
+                  | otherwise = 0.1
+
+perderPeso :: Peso -> Raton -> Raton
+perderPeso pesoAPerder raton | (peso raton) > pesoAPerder = raton {peso = (peso raton) - pesoAPerder}
+                             | otherwise = raton {peso = 0}
+
+hierbaZort :: Hierba
+hierbaZort = (cambiarNombreA "pinky").(rejuvenecer 0).perderEnfermedades
+
+perderEnfermedades :: Raton -> Raton
+perderEnfermedades raton = raton {enfermedades = []}
+
+cambiarNombreA :: Nombre -> Raton -> Raton
+cambiarNombreA nuevoNombre raton = raton {nombre = nuevoNombre}
+
+hierbaDelDiablo :: Hierba
+hierbaDelDiablo = (perderPeso 0.1).(eliminarEnfermedades 10)
+
+eliminarEnfermedades :: Int -> Raton -> Raton
+eliminarEnfermedades cantidadDeLetrasMax raton = raton {enfermedades = (filter ((<=10).length) (enfermedades raton))}
+
 
